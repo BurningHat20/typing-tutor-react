6f23738bff8056ef8351ae3e32fe44395f00f43d
BurningHat20 <yashgohel395@gmail.com>
2024-07-18 10:38:30 +0530

    refactor: Update SignIn component to use useClerk hook and remove unnecessary code

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { SignIn, useClerk } from '@clerk/clerk-react';
+import { SignIn } from '@clerk/clerk-react';
 
 const SignInComponent = () => {
-  const { signIn } = useClerk();
-
   return (
     <div className="flex justify-center items-center my-8">
-      <SignIn 
-        signInUrl="/sign-in"
-        afterSignInUrl="/lessons"
-        routing="path"
-        afterSignIn={(user) => {
-          console.log("Sign in successful", user);
-          // You can add additional logic here if needed
-        }}
-      />
+      <SignIn />
     </div>
   );
 };
