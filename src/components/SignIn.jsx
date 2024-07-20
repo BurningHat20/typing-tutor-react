f04991c524457424b29a2a216f5f632353a28884
BurningHat20 <yashgohel395@gmail.com>
2024-07-20 11:05:52 +0530

    refactor: Remove handleAuthenticated function from SignIn component
    The handleAuthenticated function is no longer needed in the SignIn component as the sign-in functionality is now handled by the useClerk hook. This change simplifies the code and improves the overall structure of the application.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { SignIn } from '@clerk/clerk-react';
 
 const SignInComponent = () => {
-  const handleAuthenticated = () => {
-    window.location.href = '/lessons';
-  };
-
   return (
     <div className="flex justify-center items-center my-8">
-      <SignIn onAuthenticated={handleAuthenticated} />
+      <SignIn />
     </div>
   );
 };
